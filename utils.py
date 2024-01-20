dad81d461cf20ea350fe1b5559d9c47b79bc8f15
Kiryo Ryuichi <kiryo@ms.k.u-tokyo.ac.jp>
2024-01-20 18:06:11 +0900

    Refactor code for consistency and readability

--- utils.py
+++ utils.py
@@ -1,17 +1,20 @@
-import torch
-import numpy as np
-import random
 import os
+import random
+
+import numpy as np
+import torch
 import torch.nn.functional as F
-from sklearn.metrics import roc_auc_score, average_precision_score
+from sklearn.metrics import average_precision_score, roc_auc_score
+
 
 def fix_random_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)  
+    torch.cuda.manual_seed_all(seed)
     np.random.seed(seed)  # Numpy module
     random.seed(seed)  # Python random module
     torch.backends.cudnn.deterministic = True
+
 
 def train(data_loader, model, device, loss_fn, optimizer, schedular):
     model.train()
@@ -27,9 +30,10 @@ def train(data_loader, model, device, loss_fn, optimizer, schedular):
         optimizer.step()
 
         loss_total = loss_total + loss.item()
-    
-    schedular.step()
+
+    # schedular.step()
     return loss_total / len(data_loader)
+
 
 def validate(data_loader, model, device, loss_fn):
     model.eval()
@@ -56,30 +60,34 @@ def validate(data_loader, model, device, loss_fn):
 
     threshold = 0.5
     preds = np.zeros_like(predicted_scores, dtype=int)
-    preds[predicted_scores>=threshold] = 1
-    corrects = np.sum(preds==labels)
-    TP = np.sum(preds[labels==1]==1)
-    FP = np.sum(labels[preds==1]==0)
-    FN = np.sum(labels[preds==0]==1)
+    preds[predicted_scores >= threshold] = 1
+    corrects = np.sum(preds == labels)
+    TP = np.sum(preds[labels == 1] == 1)
+    FP = np.sum(labels[preds == 1] == 0)
+    FN = np.sum(labels[preds == 0] == 1)
 
     acc = float(corrects) / len(predicted_scores)
-    precision = float(TP) / (TP+FP+1e-8)
-    recall = float(TP) / (TP+FN+1e-8)
-    f1 = 2 * precision * recall / (precision+recall+1e-8)
+    precision = float(TP) / (TP + FP + 1e-8)
+    recall = float(TP) / (TP + FN + 1e-8)
+    f1 = 2 * precision * recall / (precision + recall + 1e-8)
 
     auc = roc_auc_score(labels, predicted_scores)
     ap = average_precision_score(labels, predicted_scores)
-    return loss/len(data_loader), acc, precision, recall, f1, auc, ap
+    return loss / len(data_loader), acc, precision, recall, f1, auc, ap
+
 
 def mkdirs(path):
     dirs = os.path.split(path)[0]
     if not os.path.exists(dirs):
         os.makedirs(dirs)
 
+
 def save_chekpt(path, epoch, model, optimizer, schedular):
     mkdirs(path)
-    all_states = {'epoch': epoch,
-        'model': model.state_dict(),
-        'optimizer': optimizer.state_dict(),
-        'scheduler': schedular.state_dict()}
+    all_states = {
+        "epoch": epoch,
+        "model": model.state_dict(),
+        "optimizer": optimizer.state_dict(),
+        "scheduler": schedular.state_dict(),
+    }
     torch.save(obj=all_states, f=path)
